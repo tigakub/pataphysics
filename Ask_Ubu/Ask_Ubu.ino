8c6b1896216a2b763b9fcd2d6aa055223e8926eb
Fabrice Florin <fabriceflorin@users.noreply.github.com>
2015-09-10 20:26:24 -0700

    Updated 'Ask Ubu' code
    Removed all unused functions from the previous version of the code.

--- Ask_Ubu/Ask_Ubu.ino
+++ Ask_Ubu/Ask_Ubu.ino
@@ -1,16 +1,31 @@
-/* 
-This is a test sketch for the Adafruit assembled Motor Shield for Arduino v2
-It won't work with v1.x motor shields! Only for the v2's with built in PWM
-control
+/****************************************************
 
-For use with the Adafruit Motor Shield v2 
+Arduino code for Ask Ubu, an interactive art "wonderbox"
+for the Pataphysical Slot Machine.
+
+Interactive specification:
+http://bit.ly/wonderbox-spec
+
+Wonderbox schematics:
+http://bit.ly/wonderbox-schema
+
+About the Pataphysical Slot Machine:
+http://pataphysics.us
+
+Last updated on September 10, 2015.
+
+Written by Fabrice Florin, based on free libraries from Arduino and Adafruit, with guidance by Donald Day.
+
+This free software is licensed under GPLv2.
+ 
+It started as a test sketch for the Adafruit Motor Shield v2 
 ---->	http://www.adafruit.com/products/1438
 
 This sketch creates a fun motor party on your desk *whiirrr*
-Connect a unipolar/bipolar stepper to M3/M4
 Connect a DC motor to M1
 Connect a hobby servo to SERVO1
-*/
+  
+ ****************************************************/
  
 #include <Wire.h>
 #include <Adafruit_MotorShield.h>
@@ -22,15 +37,11 @@ Adafruit_MotorShield AFMS = Adafruit_MotorShield();
 // Or, create it with a different I2C address (say for stacking)
 // Adafruit_MotorShield AFMS = Adafruit_MotorShield(0x61); 
 
-// Connect a stepper motor with 200 steps per revolution (1.8 degree)
-// to motor port #2 (M3 and M4)
-Adafruit_StepperMotor *myStepper = AFMS.getStepper(200, 2);
-// And connect a DC motor to port M1
+// Connect a DC motor to port M1
 Adafruit_DCMotor *myMotor = AFMS.getMotor(1);
 
-// We'll also test out the built in Arduino Servo library
+// We'll also test out the built-in Arduino Servo library
 Servo servo1;
-Servo servo2;
 
 void setup() {
   Serial.begin(9600);           // set up Serial library at 9600 bps
@@ -47,8 +58,6 @@ void setup() {
   myMotor->setSpeed(200);
   myMotor->run(RELEASE);
   
-  // setup the stepper
-  myStepper->setSpeed(10);  // 10 rpm   
 }
 
 int i;
@@ -56,34 +65,26 @@ void loop() {
   myMotor->run(FORWARD);
   for (i=0; i<255; i++) {
     servo1.write(map(i, 0, 255, 0, 100));
-    servo2.write(map(i, 0, 255, 0, 100));
     myMotor->setSpeed(i);  
-    myStepper->step(1, FORWARD, INTERLEAVE);
     delay(3);
  }
  
  for (i=255; i!=0; i--) {
     servo1.write(map(i, 0, 255, 0, 100));
-    servo2.write(map(i, 0, 255, 0, 100));
     myMotor->setSpeed(i);  
-    myStepper->step(1, BACKWARD, INTERLEAVE);
     delay(3);
  }
  
   myMotor->run(BACKWARD);
   for (i=0; i<255; i++) {
     servo1.write(map(i, 0, 255, 0, 100));
-    servo2.write(map(i, 0, 255, 0, 100));
     myMotor->setSpeed(i);  
-    myStepper->step(1, FORWARD, DOUBLE);
     delay(3);
  }
  
   for (i=255; i!=0; i--) {
     servo1.write(map(i, 0, 255, 0, 100));
-    servo2.write(map(i, 0, 255, 0, 100));
     myMotor->setSpeed(i);  
-    myStepper->step(1, BACKWARD, DOUBLE);
     delay(3);
  }
 }
