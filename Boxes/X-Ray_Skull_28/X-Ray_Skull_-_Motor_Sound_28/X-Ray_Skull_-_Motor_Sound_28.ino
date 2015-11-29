dadc3d4ac5cd18d47f7c518aca377c9b898f003b
Fabrice Florin <fabriceflorin@users.noreply.github.com>
2015-11-29 14:13:40 -0800

    Updated sound playback code
    Updated sound client code for the X-Ray Skull wonderbox: the delay is
    now equal to 200 milliseconds multiplied by the box number, so the
    boxes don’t all call the sound server at the same time. Also added some
    code to detach the servo when it is not in use, to avoid straining it.

--- Boxes/X-Ray_Skull_28/X-Ray_Skull_-_Motor_Sound_28/X-Ray_Skull_-_Motor_Sound_28.ino
+++ Boxes/X-Ray_Skull_28/X-Ray_Skull_-_Motor_Sound_28/X-Ray_Skull_-_Motor_Sound_28.ino
@@ -15,10 +15,10 @@ http://pataphysics.us
 Wonderbox specifications:
 http://bit.ly/wonderbox-spec
 
-Last updated on September 27, 2015.
+Last updated on November 29, 2015.
 
-Written by Jean Bolte, with help from Donald Day and Fabrice Florin, 
-based on free libraries from Arduino, Adafruit and Barraganstudio.com. 
+Written by Jean Bolte and Fabrice Florin, 
+based on free libraries from Arduino and Adafruit. 
 Sound playback code by Donald Day and Tim Pozar.
 
 This free software is licensed under GPLv2.
@@ -33,11 +33,11 @@ This free software is licensed under GPLv2.
 // Connect the red wire from the pataphysical bus to the 5V pin on Arduino
 // Connect the black wire from the pataphysical bus to any ground pin on Arduino
 // Connect the green wire from the pataphysical bus to Analog 5 (i2c clock) 
-// Connect the blue wire from the pataphysical bus to Analog 4 (i2c data) 
+// Connect the blue (or yellow) wire from the pataphysical bus to Analog 4 (i2c data) 
 
 Adafruit_MCP23008 mcp; // instantiate Adafruit_MCP23008 mcp
 
-const int box_button = 12 ; // the switch for the whole box may be placed on pin 12 -- it is triggered when you open the box. 
+const int box_button = 12 ; // the switch for the whole box is on pin 12 -- it is triggered when you open the box. 
 
 Servo myservo;  // create servo object to control a servo 
                 // a maximum of eight servo objects can be created 
@@ -56,13 +56,22 @@ int quietValue = 0;  // TRK0 means stop playing
 
 void setup() 
 { 
-
+  
+Serial.begin(9600); // enables serial connection
+  
+// first position the skull's head in the center
+ 
 myservo.attach(9);  // attaches the servo on pin 9 to the servo object 
 
-Serial.begin(9600); // enables serial connection
-    
-  delay(5000); //wait five seconds after powerup    
-  mcp.begin();      // use default address 0, based at 0x20
+myservo.write(30);   // tell servo to go to the center, while we wait for sound server delay. 
+
+myservo.detach();  // detaches the servo to avoid straining it while we wait
+  
+// then play the sound
+
+
+delay(200*songValue); // wait a few seconds before connecting with the sound server.    
+mcp.begin();      // use default address 0, based at 0x20
 
 }
 
@@ -77,26 +86,48 @@ void loop()
   // if it is, the buttonState is HIGH, nc button:
   if (buttonState != oldbuttonState) {     
     if (buttonState == HIGH) {     
-//    mcp.begin();      // use default address 0, based at 0x20
+
+    // play the sound
       mcp.writeGPIO(songValue);
-    }
-    else {
-      mcp.writeGPIO(quietValue);
-    }
-  }
-  oldbuttonState = buttonState;
- 
- 
+      
+    // then move the skull's head
+  
+  myservo.attach(9);  // attaches the servo on pin 9 to the servo object 
+  
+  
   for(pos = 0; pos < 60; pos += 1)  // goes from 0 degrees to 180 degrees 
   {                                  // in steps of 1 degree 
-    myservo.write(pos);              // tell servo to go to position in variable 'pos' 
-    delay(50);                       // waits 15ms for the servo to reach the position 
+
+  myservo.write(pos);              // tell servo to go to position in variable 'pos' 
+  
+  delay(50);                       // wait for the servo to reach the position 
+  
   } 
   for(pos = 60; pos>=1; pos-=1)     // goes from 180 degrees to 0 degrees 
   {                                
     myservo.write(pos);              // tell servo to go to position in variable 'pos' 
-    delay(50);                       // waits 15ms for the servo to reach the position 
+    delay(50);                       // wait for the servo to reach the position 
   } 
+  
+   myservo.detach();  // detaches the servo to avoid straining it while we wait
+    
+    }
+    else {
+      mcp.writeGPIO(quietValue);
+      
+    
+    // position the skull's head in the center
+ 
+    myservo.attach(9);  // attaches the servo on pin 9 to the servo object 
+
+    myservo.write(30);  // tell servo to go to the center, while we wait for sound server delay. 
+
+    myservo.detach();  // detaches the servo to avoid straining it while we wait
+     
+    }
+  }
+  oldbuttonState = buttonState;
+ 
 }
 
 
