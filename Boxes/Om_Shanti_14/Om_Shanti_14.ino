4e094f507d2de8b338658c4b58a6d8f901deca04
Fabrice Florin <fabriceflorin@users.noreply.github.com>
2016-01-24 12:14:37 -0800

    Updated Om Shanti to i2c
    Updated Om Shanti to make it sing again, using the new i2c library.

--- Boxes/Om_Shanti_14/Om_Shanti_14.ino
+++ Boxes/Om_Shanti_14/Om_Shanti_14.ino
@@ -3,9 +3,9 @@
  Arduino code for the Om Shanti Wonderbox, an interactive art scene.
  
  In this scene, three 'eye flowers' sing 'Om Shanti'. 
- We originally played sounds using the Adafruit Waveshield, 
- but we now play them from a separate sound server over our i2c bus, 
- using an MCP23008 i2c expander and MusicMaker.
+ 
+ We now play the 'Om Shanti' song from a separate sound server 
+ over our i2c bus, using the i2C library, an MCP23008 i2c expander and MusicMaker.
  
  Three white LEDs in the ceiling light up the scene (5v).
  Each eye flower has an  RGB LED that automatically cycles through
@@ -26,7 +26,7 @@ http://bit.ly/wonderbox-schema
 Photos and info about Pataphysical Studios:
 http://pataphysics.us
 
- Last updated on November 29, 2015.
+ Last updated on January 24, 2016.
  Written by Fabrice Florin, with guidance from Donald Day and Anselm Hook, 
  based on free libraries from Arduino, Adafruit and others. 
  Sound playback code by Donald Day and Tim Pozar.
@@ -42,30 +42,31 @@ This free software is licensed under GPLv2.
 
 // BUTTON SETTINGS
 
-const int buttonPin = 9;     // the number of the button pin
-int buttonState = 0;         // variable for reading the button status
+const int buttonPin = 12;    // the number of the pushbutton pin
+int thisPin = 10;             // the LED pin
+int buttonState = 0;         // variable for reading the pushbutton status
 int oldbuttonState = 0;      // for button changes
+int songValue = 14;  // Play Track 14 (Box #14)
+int quietValue = 0;  // TRK0 means stop playing 
 
 
 // SOUND SERVER SETTINGS
 
-#include <Wire.h>
-#include "Adafruit_MCP23008.h"
+// #include <Wire.h>
+#include <I2C.h>
 
 /* Wiring instructions:
 - Connect the red wire from the pataphysical bus to the 5V pin on Arduino
 - Connect the black wire from the pataphysical bus to any ground pin on Arduino
 - Connect the green wire from the pataphysical bus to Analog 5 (i2c clock) (goes to pin #1 of the expander)
 - Connect the blue wire from the pataphysical bus to Analog 4 (i2c data) (goes to pin #2 of the expander)
+- Connect the door button to pin 12 of the Arduino (add a 2.2k pull-down resistor and connect it to ground)
+- Connect the other wires from the ceiling LEDs to the 5V pin of the Arduino
+- Connect the wires from the ceiling LEDs to pin 10 of the Arduino (also connected to ground on the terminal)
+- Connect the wires from the eye LEDs to the 3.3V pin of the Arduino
+- Connect the wires from the eye LEDs to one of the ground pins
 The code below is a basic toggle test for i/o expansion. It flips pin #0 of a MCP23008 i2c pin expander up and down. 
 */
-
-Adafruit_MCP23008 mcp;
-
-int songValue = 14;  // Play Track 14 (Box #14)
-int quietValue = 0;  // TRK0 means stop playing
-
-
 
 
 ////////////////////////////////////////////
@@ -82,17 +83,14 @@ int quietValue = 0;  // TRK0 means stop playing
   pinMode(buttonPin, INPUT);
   
  // Setup LEDs
-   // initialize each LED pin as an output, turn on:
-  for (int thisPin = 6; thisPin < 9; thisPin++)  {
+   // initialize LED pin as an output, turn on:
    pinMode(thisPin, OUTPUT);      // sets the pin as output
    digitalWrite(thisPin, LOW);    // sets the LED off 
-  }
 
  // Setup Sound Server
    delay(200*songValue); // wait a few seconds to call the sound server
-  mcp.begin();      // use default address 0, based at 0x20
+  I2c.begin();
 }
-
 
 
 ////////////////////////////////////////////
@@ -103,37 +101,27 @@ int quietValue = 0;  // TRK0 means stop playing
 void loop() {
   oldbuttonState = buttonState;
   buttonState = digitalRead(buttonPin);
-  // check if the button is pressed.
+  // check if the pushbutton is pressed.
   // if it is, the buttonState is HIGH, nc button:
   if (buttonState != oldbuttonState) {     
     if (buttonState == HIGH) {     
-      
-      
-    // play sound     
-     mcp.writeGPIO(songValue);
-      delay(200);            // delay 200 milliseconds
+      I2c.write(32, 9, songValue);
+
       Serial.println ("Playing song with Sound Server.");
       
       // turn LEDs on
-        for (int thisPin = 6; thisPin < 9; thisPin++)  {
        digitalWrite(thisPin, HIGH);    // turn the LEDs on 
-      }
     }
     else {
          
-    // stop sound
-     mcp.writeGPIO(quietValue);
-      delay(200);            // delay 200 milliseconds
+      I2c.write(32, 9, quietValue);
       Serial.println ("Stopping sound from Sound Server.");
       
       // turn LEDs off
-        for (int thisPin = 6; thisPin < 9; thisPin++)  {
         digitalWrite(thisPin, LOW);    // turn the LEDs off 
-        }
-     }
    }
+  }
   oldbuttonState = buttonState;
   delay(20);
 }
-
 
