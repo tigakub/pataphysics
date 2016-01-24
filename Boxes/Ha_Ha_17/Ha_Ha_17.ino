b0f6ca013c1e2bc193b06a11ea7bdb7bd2ed5e8f
Tim Pozar <github@lns.com>
2016-01-23 17:36:39 -0800

    Formating and changed "box_button" to "buttonPin" to make this work.

--- Boxes/Ha_Ha_17/Ha_Ha_17.ino
+++ Boxes/Ha_Ha_17/Ha_Ha_17.ino
@@ -1,5 +1,4 @@
 /****************************************************
-
 Arduino code for the Ha Ha box, an interactive art "wonderbox"
 for the Pataphysical Slot Machine.
 
@@ -18,11 +17,9 @@ Written by Fabrice Florin and Freddy Hahne,
 based on free libraries from Arduino, Adafruit and others. 
 Sound playback code by Donald Day and Tim Pozar.
 
-This free software is licensed under GPLv2.
-  
- ****************************************************/
+This free software is licensed under GPLv2.  
+****************************************************/
  
-
 #include <Wire.h> // to connnect with other boxes with the i2c protocol, for sound playback
 #include "Adafruit_MCP23008.h" // to connnect with the i2c expander, for sound playback
 // Download the latest Adafruit_MCP23008 code here: https://github.com/adafruit/Adafruit-MCP23008-library
@@ -34,7 +31,7 @@ This free software is licensed under GPLv2.
 
 Adafruit_MCP23008 mcp; // instantiate Adafruit_MCP23008 mcp
 
-const int box_button = 12; // the switch for the whole box may be placed on pin 12 -- it is triggered when you open the box. 
+const int buttonPin = 12; // the switch for the whole box may be placed on pin 12 -- it is triggered when you open the box. 
 int buttonState = 0;         // variable for reading the pushbutton status
 int oldbuttonState = 0;      // for button changes
 
@@ -44,8 +41,6 @@ int quietValue = 0;  // TRK0 means stop playing
 const int led_1 = 10; // LED group 1 is on pin 10
 const int led_2 = 11; // LED group 2 is on pin 11
 
-
-
 //********************  SETUP    **********************
 
 void setup() {  
@@ -54,24 +49,19 @@ void setup() {
   mcp.begin();      // use default address 0, based at 0x20
 }
 
-
 //********************  MAIN LOOP    **********************
 
- 
 void loop() 
 { 
- 
   // First ask the server to play a sound if the button has been pressed.
-  
   oldbuttonState = buttonState;
-  buttonState = digitalRead(box_button);
+  buttonState = digitalRead(buttonPin);
   // check if the pushbutton is pressed.
   // if it is, the buttonState is HIGH, nc button:
   if (buttonState != oldbuttonState) 
   {     
     if (buttonState == HIGH) 
     {     
-      
       mcp.writeGPIO(songValue);
     }
     else 
@@ -81,10 +71,9 @@ void loop()
   }
   oldbuttonState = buttonState;
   delay(20);
- 
 
-// Then make the two groups of LEDs blink on and off -- feel free to tweak the timing as needed 
- 
+// Then make the two groups of LEDs blink on and off -- 
+// feel free to tweak the timing as needed  
   digitalWrite(led_1, HIGH);   // turn the LED on (HIGH is the voltage level)
   delay(1000);               // wait for a second
   digitalWrite(led_2, LOW);    // turn the LED off by making the voltage LOW
@@ -93,7 +82,4 @@ void loop()
   delay(1000);               // wait for a second
   digitalWrite(led_1, LOW);    // turn the LED off by making the voltage LOW
   delay(1000);               // wait for a second
-    
- 
 }
-
