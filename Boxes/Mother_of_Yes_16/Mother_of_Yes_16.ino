39822b56843b01c3b1ffef6b7fdb40ca0b3ab5a7
Fabrice Florin <fabriceflorin@users.noreply.github.com>
2016-02-13 22:06:57 -0800

    Last Sound Update: Mother of Yes
    Updated the sound playback code for Mother of Yes. This was the last
    wonderbox that needed sounds for the Pataphysical Slot Machine. Yay!
    It’s a great feeling to finally have all boxes playing sounds, after
    working on this i2c network for over a year.

--- Boxes/Mother_of_Yes_16/Mother_of_Yes_16.ino
+++ Boxes/Mother_of_Yes_16/Mother_of_Yes_16.ino
@@ -11,7 +11,7 @@ http://pataphysics.us
 Wonderbox specifications:
 http://bit.ly/wonderbox-spec
 
-Last updated on November 28, 2015.
+Last updated on February 13, 2016.
 
 Written by Fabrice Florin, based on free libraries from Arduino, Adafruit and others. 
 Sound playback code by Donald Day and Tim Pozar.
@@ -19,8 +19,6 @@ Sound playback code by Donald Day and Tim Pozar.
 This free software is licensed under GPLv2.
 ****************************************************/
 
-// #include <Wire.h>
-// #include "Adafruit_MCP23008.h"
 #include <I2C.h>
 
 /* Wiring instructions:
@@ -29,18 +27,8 @@ For the Arduino or Diavolino clients in each Wonderbox:
 - Connect the black wire from the pataphysical bus to any ground pin on Arduino
 - Connect the green wire from the pataphysical bus to Analog 5 (i2c clock) (goes to pin #1 of the expander)
 - Connect the blue wire from the pataphysical bus to Analog 4 (i2c data) (goes to pin #2 of the expander)
-For the Arduino sound server with the MCP23008 i2c expander: (ignore these if you are only working on the clients)
-- Connect pin #1 of the expander to Analog 5 (i2c clock - SCL) (green wire on the pataphysical bus)
-- Connect pin #2 of the expander to Analog 4 (i2c data - SDA) (blue wire on the pataphysical bus, yellow wire on sound server)
-- Connect pins #3, 4 and 5 of the expander to ground (address selection)
-- Connect pin #6 and 18 of the expander to 5V (power and reset disable)
-- Connect pin #9 of the expander to ground (common ground)
-- Output #0 is on pin 10 so connect an LED or whatever from that to ground
-The code below is a basic toggle test for i/o expansion. It flips pin #0 of a MCP23008 i2c pin expander up and down. 
 */
 
-// writeGPIO(data);
-// Adafruit_MCP23008 mcp;
 
 const int buttonPin = 12;     // the number of the pushbutton pin
 int buttonState = 0;         // variable for reading the pushbutton status
@@ -56,7 +44,6 @@ void setup() {
   Serial.println("Start of setup.");
   pinMode(buttonPin, INPUT);
   delay(200*songValue); // waitby#
-  // mcp.begin();      // use default address 0, based at 0x20
   I2c.begin();
   Serial.println("End of setup.");
 }
@@ -69,13 +56,10 @@ void loop() {
   // if it is, the buttonState is HIGH, nc button:
   if (buttonState != oldbuttonState) {     
     if (buttonState == HIGH) {     
-      // mcp.begin();      // use default address 0, based at 0x20
-      // mcp.writeGPIO(songValue);
-      I2c.write(32, 9, songValue);
+       I2c.write(32, 9, songValue);
       Serial.println("Door is open.");
     }
     else {
-      // mcp.writeGPIO(quietValue);
       I2c.write(32, 9, quietValue);
       Serial.println("Door is closed.");
     }
